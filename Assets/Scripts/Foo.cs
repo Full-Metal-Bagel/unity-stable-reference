0034eb53fef0544dbaca1981861ec873c5422547
quabug <quabug@gmail.com>
2025-05-13 11:02:52 +0900

    fix sample

--- Assets/Scripts/Foo.cs
+++ Assets/Scripts/Foo.cs
@@ -8,21 +8,25 @@ public interface IFoo
     void Print();
 }
 
-[Guid("A267E890-16F1-406E-8E00-8FB9588F3135"), StableWrapperCodeGen]
+[Serializable, Guid("A267E890-16F1-406E-8E00-8FB9588F3135"), StableWrapperCodeGen]
 public class FooImpl1 : IFoo
 {
+    public int Value;
+    
     public void Print()
     {
-        Debug.Log("FooImpl1");
+        Debug.Log("FooImpl1 " + Value);
     }
 }
 
-[Guid("3DDC5AB5-736A-4691-9208-B5641C9E7DE5"), StableWrapperCodeGen]
+[Serializable, Guid("3DDC5AB5-736A-4691-9208-B5641C9E7DE5"), StableWrapperCodeGen]
 public class FooImpl2 : IFoo
 {
+    public float Value;
+    
     public void Print()
     {
-        Debug.Log("FooImpl2");
+        Debug.Log("FooImpl2 " + Value);
     }
 }
 
